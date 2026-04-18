f444c08febc67031ae5ac50f3eff304ab945ec11
MuhammadZahidullah <zahidullahk451@gmail.com>
2026-04-19 00:06:50 +0500

    Update firestore_list_screen.dart
    fetching data from firstore to UI

--- lib/ui/firestore/firestore_list_screen.dart
+++ lib/ui/firestore/firestore_list_screen.dart
@@ -1,10 +1,11 @@
-import 'package:firbase_with_asif/post/add_post_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firbase_with_asif/post/add_post_screen.dart';
 import 'package:firbase_with_asif/ui/auth/login_screen.dart';
 import 'package:firbase_with_asif/ui/firestore/add_firestore_screen.dart';
 import 'package:firbase_with_asif/utils/utilities.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
+//import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class FirestoreListScreen extends StatefulWidget {
@@ -19,6 +20,8 @@ class _FirestoreListScreenState extends State<FirestoreListScreen> {
   //final searchFilterController = TextEditingController();
   final editController = TextEditingController();
   //final firebaseRef = FirebaseDatabase.instance.ref('post');
+  final fireStore = FirebaseFirestore.instance.collection('Users').snapshots();
+  CollectionReference ref = FirebaseFirestore.instance.collection('Users');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,13 +64,66 @@ class _FirestoreListScreenState extends State<FirestoreListScreen> {
         children: [
           SizedBox(height: 15),
 
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return ListTile(title: Text('Muhammad Zahidullah'));
-              },
-            ),
+          StreamBuilder<QuerySnapshot>(
+            stream: fireStore,
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting)
+                    return CircularProgressIndicator();
+
+                  if (snapshot.hasError) return Text('Some Error');
+
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          onTap: () {},
+                          title: Text(
+                            snapshot.data!.docs[index]['title'].toString(),
+                          ),
+                          subtitle: Text(snapshot.data!.docs[index].id),
+                          trailing: PopupMenuButton(
+                            itemBuilder: (context) => [
+                              PopupMenuItem(
+                                value: 1,
+                                child: ListTile(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    showMyDiolog(
+                                      snapshot.data!.docs[index]['title']
+                                          .toString(),
+                                      snapshot.data!.docs[index]['id']
+                                          .toString(),
+                                    );
+                                  },
+                                  leading: Icon(Icons.edit),
+                                  title: Text('Edit'),
+                                ),
+                              ),
+                              PopupMenuItem(
+                                value: 1,
+                                child: ListTile(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    ref
+                                        .doc(
+                                          snapshot.data!.docs[index]['id']
+                                              .toString(),
+                                        )
+                                        .delete();
+                                  },
+                                  leading: Icon(Icons.delete_forever_outlined),
+                                  title: Text('Delete'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
           ),
         ],
       ),
@@ -91,11 +147,29 @@ class _FirestoreListScreenState extends State<FirestoreListScreen> {
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
+                ref
+                    .doc(id)
+                    .update({'title': editController.text})
+                    .then((value) {
+                      Utilities().toastMessage('Updated');
+                    })
+                    .onError((error, StackTrace) {
+                      Utilities().toastMessage(error.toString());
+                    });
               },
               child: Text('Cancel'),
             ),
             TextButton(
               onPressed: () {
+                ref
+                    .doc(id)
+                    .update({'title': editController.text})
+                    .then((value) {
+                      Utilities().toastMessage('Updated');
+                    })
+                    .onError((error, stackTrace) {
+                      Utilities().toastMessage(error.toString());
+                    });
                 Navigator.pop(context);
               },
               child: Text('Update'),
